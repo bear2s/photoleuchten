48230ab46a32a279e91399a22092d1225ca8213c
Steffen Baer <stbaer.mail@gmail.com>
2017-04-08 23:14:27 +0800

    removed lazyload; updated deps; added new images

---
+++ scripts/mvImages.js
@@ -0,0 +1,12 @@
+const shell = require('shelljs')
+const path = require('path')
+
+const baseDir = path.resolve(__dirname, '../')
+
+console.log(baseDir)
+console.log(` Removing ${baseDir}/static/img`)
+shell.rm('-Rf', `${baseDir}/static/img`)
+
+console.log(` images to ${baseDir}/static/img`)
+shell.mkdir(`${baseDir}/static/img`)
+shell.cp('-R', `${baseDir}/node_modules/pl-images/dist/*`, `${baseDir}/static/img`)

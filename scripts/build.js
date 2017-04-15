03c208986988b1de49ef86db412679eec8012c5f
Steffen Baer <stbaer.mail@gmail.com>
2017-04-15 09:50:50 +0800

    build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,11 @@ const path = require('path')
 
 const baseDir = path.resolve(__dirname, '../')
 
+shell.echo('\n -- Starting build -- \n')
+
 shell.exec('npm run generate')
 shell.rm('-rf', `${baseDir}/docs`)
 shell.cp('-R', `${baseDir}/dist`, `${baseDir}/docs`)
 shell.exec('echo "photoleuchten.com" > docs/CNAME')
-shell.exec('git add -A && git commit -am "updated docs folder"')
+
+shell.echo('\n -- Build complete -- \n')

66f3e767324ee8f10500b16530a7e456d004d150
Steffen Baer <stbaer.mail@gmail.com>
2017-06-18 09:04:14 +0800

    added text; minor style change; updated vue-i18n pkg;

--- components/QuadObjects.vue
+++ components/QuadObjects.vue
@@ -16,6 +16,9 @@
     </div>
     <br>
     <div class="row middle-xs">
+      <div class="col-xs-12">
+        <div v-html="$t('quad_desc')"></div>
+      </div>
       <div class="col-xs-8">
         <h4>{{$t('products.specs')}}</h4>
         <p v-html="$t('specs_description')"></p>
@@ -139,6 +142,11 @@
         en: {
           'battery_powered': 'battery-powered',
           'line_powered': 'line-powered',
+          quad_desc: `
+<p>The square light boxes are very flexible - they can be hung on the wall, placed on the floor or on a shelf. They are battery powered and do not need any power outlet.</p>
+<p>As motifs, there are detailed photographs, e.g. "Salt & Pepper" - salt and pepper grains in the light - or different nature photographs.</p>
+<p>The motif is printed on an acrylic glass plate and fixed from the inside with picture clips. It can be replaced easily. Each frame is a single piece of carpentry.</p>
+`,
           'specs_description': `
 Handmade individual item<br/>
 <span class="desc_point">Frame:</span> white painted wood<br/>
@@ -152,6 +160,12 @@ Motif can be easily changed<br/>
         de: {
           battery_powered: 'batteriebetrieben',
           line_powered: 'netzbetrieben',
+          quad_desc:
+`
+<p>Die quadratischen Leuchtkästen sind sehr flexibel - sie können an die Wand gehängt, auf den Boden oder auf ein Regal gestellt werden. Sie sind batteriebetrieben und brauchen keine Steckdose.</p>
+<p>Als Motive gibt es Detailfotografien, wie z.B. "Salt & Pepper" -Salz- und Pfefferkörner im Licht oder verschiedene Naturaufnahmen.</p>
+<p>Das Motiv ist auf einer Acrylglasplatte gedruckt und von innen mit Bilderklemmen befestigt. Es kann jederzeit ausgetauscht werden. Jeder Rahmen ist ein schreinergefertigtes Einzelstück.</p>
+`,
           specs_description: `
 Handgefertigtes Einzelstück<br/>
 <span class="desc_point">Rahmen:</span> Holzrahmen weiss lackiert<br/>

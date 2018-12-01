c19362577b97f5d8a729fb9bc5add5d24db7e213
Steffen Baer <stbaer.mail@gmail.com>
2018-12-01 15:53:43 +0800

    fix canonical

--- plugins/vueSetup.js
+++ plugins/vueSetup.js
@@ -3,6 +3,14 @@ import PlImg from '~/components/PlImg.vue'
 import ToggleButton from '~/components/ToggleButton.vue'
 
 Vue.mixin({
+  head () {
+    const path = this.$route.path.replace(/\/$/, '') // Remove trailing /
+    return {
+      link: [
+        { rel: 'canonical', href: `https://photoleuchten.com${path}` }
+      ]
+    }
+  },
   components: {
     PlImg, ToggleButton
   },

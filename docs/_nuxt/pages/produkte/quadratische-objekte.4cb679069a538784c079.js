dbd555d29a9c5354c22e15823250cc415f1de1d9
Steffen Baer <stbaer.mail@gmail.com>
2018-12-27 14:49:46 +0700

    updated docs folder

--- docs/_nuxt/pages/produkte/quadratische-objekte.4cb679069a538784c079.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{254:function(e,t,n){"use strict";n.r(t);var o={fetch:function(e){return(0,e.redirect)(301,"/produkte/leuchtkaesten")}},u=n(3),i=Object(u.a)(o,void 0,void 0,!1,null,null,null);i.options.__file="quadratische-objekte.vue";t.default=i.exports}}]);
---
+++ docs/_nuxt/pages/produkte/runde-objekte.a5092878ef1c38217ce2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{144:function(e,o,t){"use strict";t.r(o);var n={fetch:e=>(0,e.redirect)(301,"/produkte/lichtobjekte")},i=t(4),u=Object(i.a)(n,void 0,void 0,!1,null,null,null);u.options.__file="runde-objekte.vue";o.default=u.exports}}]);

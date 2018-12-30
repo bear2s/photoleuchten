f0a17dfc38d15e52ed58fb8e180c88c215345822
Steffen Baer <stbaer.mail@gmail.com>
2018-12-30 10:07:46 +0800

    updated docs folder

---
+++ docs/_nuxt/pages/en/products/framed-objects.02b30e1f42379f34addc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{145:function(e,o,t){"use strict";t.r(o);var n={fetch:e=>(0,e.redirect)(301,"/en/products/lightboxes")},i=t(4),s=Object(i.a)(n,void 0,void 0,!1,null,null,null);s.options.__file="framed-objects.vue";o.default=s.exports}}]);
